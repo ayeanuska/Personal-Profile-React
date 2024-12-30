6bbfed63cb279022120b066841b420b86c38200d
Anuska <anuskasth2022@gmail.com>
2024-12-30 20:02:39 +1100

    responsive hero ssection using react bootstrap

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,72 +1,69 @@
 import React from "react";
+import { Container, Row, Col, Button } from "react-bootstrap";
 import anu from "../assets/anu.jpeg";
 
 const Hero = () => {
   return (
     <>
-      <section class="hero-section container" id="hero">
-        <div class="grid hero">
-          <div class="left flex">
-            <div>
-              Hello I `am <br />
-              <span>Anuska Shrestha</span>
-            </div>
-            <div class="tag">Full stack developer</div>
-            <p>
-              with a creative mindset, currently learning to become a skilled
-              full-stack developer. Always exploring new technologies and
-              building connections
-            </p>
-            <div>
-              <a href={anu}>
-                <button>
-                  Download Cv <i class="fa-solid fa-download"></i>
-                </button>
-              </a>
-            </div>
-          </div>
-          <div class="right flex">
-            <img src={anu} alt="anuska's img" />
-          </div>
-        </div>
+      <section className="hero-section" id="hero">
+        <Container>
+          <Row className="align-items-center">
+            <Col
+              lg={6}
+              sm={12}
+              className="text-center text-lg-start order-lg-1"
+            >
+              <h1>
+                Hello I'm <span>Anuska Shrestha</span>
+              </h1>
+              <h3 className="tag">Full Stack Developer</h3>
+              <p>
+                Creative mindset, currently learning to become a skilled
+                full-stack developer. Always exploring new technologies and
+                building connections.
+              </p>
+              <Button variant="warning" href={anu} download>
+                Download CV <i className="fa-solid fa-download"></i>
+              </Button>
+            </Col>
+            <Col lg={6} sm={12} className="text-center order-lg-2">
+              <img
+                src={anu}
+                alt="Anuska"
+                className="img-fluid rounded hero-image "
+              />
+            </Col>
+          </Row>
+        </Container>
       </section>
 
       {/* <!--  banner--  --> */}
-
       <section class="banner flex container">
-        <div class="flex info-content">
-          <div class="flex-centre icon-container">
-            <i class="fa-solid fa-award"></i>
-          </div>
-          <div>
-            <span> IT</span>
-            <p>Graduation</p>
-          </div>
-        </div>
-
-        <div class="info-divider"></div>
-
-        <div class="flex info-content">
-          <div class="flex-centre icon-container">
-            <i class="fa-solid fa-award"></i>
-          </div>
-          <div>
-            <span> 5+ Projects</span>
-            <p>Completed</p>
-          </div>
-        </div>
-
-        <div class="info-divider"></div>
-
-        <div class="flex info-content">
-          <div class="flex-centre icon-container">
-            <i class="fa-solid fa-award"></i>
-          </div>
-          <div>
-            <span>1+ year</span>
-            <p>Experience</p>
-          </div>
-        </div>
+        <Container>
+          <Row>
+            <Col lg={4} sm={12} className="text-center">
+              <div>
+                <i className="fa-solid fa-award fa-3x mb-2"></i>
+                <h4>IT</h4>
+                <p>Graduation</p>
+              </div>
+            </Col>
+            <Col lg={4} sm={12} className="text-center">
+              <div>
+                <i className="fa-solid fa-code fa-3x mb-2"></i>
+                <h4>5+ Projects</h4>
+                <p>Completed</p>
+              </div>
+            </Col>
+            <Col lg={4} sm={12} className="text-center">
+              <div>
+                <i className="fa-solid fa-briefcase fa-3x mb-2"></i>
+                <h4>1+ Year</h4>
+                <p>Experience</p>
+              </div>
+            </Col>
+          </Row>
+        </Container>
       </section>
     </>
   );
