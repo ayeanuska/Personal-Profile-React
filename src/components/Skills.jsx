7731e42dd70b6d37772a4b00eaf03a175c5316a6
Anuska <anuskasth2022@gmail.com>
2025-01-10 13:06:01 +1100

    working on responsiveness

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -8,28 +8,44 @@ const Skills = () => {
         <Title title="Skills" />
         <div className="container flex skills-container" id="skills">
           <div>
-            <i className="fa-brands fa-html5" style={{ color: "orange" }}></i>
+            <i
+              className="fa-brands fa-html5 skill-box"
+              i
+              style={{ color: "orange" }}
+            ></i>
             <span> HTML5</span>
           </div>
           <div>
-            <i className="fa-brands fa-css3-alt" style={{ color: "blue" }}></i>
+            <i
+              className="fa-brands fa-css3-alt skill-box"
+              style={{ color: "blue" }}
+            ></i>
             <span> CSS3</span>
           </div>
           <div>
-            <i className="fa-brands fa-js" style={{ color: "yellow" }}></i>
+            <i
+              className="fa-brands fa-js skill-box"
+              style={{ color: "yellow" }}
+            ></i>
             <span> JavaScript</span>
           </div>
           <div>
-            <i className="fa-brands fa-react" style={{ color: "yellow" }}></i>
+            <i
+              className="fa-brands fa-react skill-box"
+              style={{ color: "yellow" }}
+            ></i>
             <span> React</span>
           </div>
           <div>
-            <i className="fa-brands fa-github" style={{ color: "black" }}></i>
-            <span>GitHUb</span>
+            <i
+              className="fa-brands fa-github skill-box"
+              style={{ color: "black" }}
+            ></i>
+            <span>GitHub</span>
           </div>
           <div>
             <i
-              className="fa-brands fa-figma"
+              className="fa-brands fa-figma skill-box"
               style={{ color: "rgba(97, 15, 113, 0.221)" }}
             ></i>
             <span>Figma</span>
